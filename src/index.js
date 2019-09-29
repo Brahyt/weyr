6f06d6d1a08540965a8cf18d0f407dbb3a44716c
Daniel Bright <brahyt@gmail.com>
2019-09-29 11:57:52 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import store from 'store.js';
+
+let element = document.getElementByClassName('js-insert');
+console.log(element)
---
+++ src/store.js
@@ -0,0 +1,20 @@
+const charClass = {
+  wizard: {},
+  rogue: {},
+  cleric: {},
+  fighter: {}
+}
+
+const race = {
+  elf: {},
+  halfElf: {},
+  orc: {},
+  halfOrc: {},
+  human: {}
+}
+
+
+export default {
+  race,
+  charClass
+};
